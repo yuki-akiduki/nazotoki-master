e1d2090a7b3f485dea207767d8ca2e2bdd74d36f
yuki-akiduki <yuki.akiduki1222@gmail.com>
2024-01-28 02:38:03 +0900

    update：正解判定とinputの空白にする処理

--- src/components/play/Game.jsx
+++ src/components/play/Game.jsx
@@ -14,20 +14,33 @@ const quizList = [
     answer: 'すいか',
   },
 ];
+const shuffleQuiz = quizList.concat();
+const quizTotal = shuffleQuiz.length;
+for (let i = quizTotal - 1; i >= 0; i--) {
+  const randomIndex = Math.floor(Math.random() * (i + 1));
+  [shuffleQuiz[i], shuffleQuiz[randomIndex]] = [shuffleQuiz[randomIndex], shuffleQuiz[i]];
+}
+console.log(shuffleQuiz);
 export default function Game() {
   const [quizNum, setQuizNum] = useState(0);
-  const [answerState, setAnswerState] = useState(0);
-  const shuffleQuiz = quizList.concat();
-  const quizTotal = shuffleQuiz.length;
-  for (let i = quizTotal - 1; i >= 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    [shuffleQuiz[i], shuffleQuiz[randomIndex]] = [shuffleQuiz[randomIndex], shuffleQuiz[i]];
-  }
-  console.log(shuffleQuiz);
+  const [correcte, setCorrect] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const changeQuizHandler = () => {
+    if (shuffleQuiz[quizNum].answer === answer) {
+      setQuizNum((prev) => ++prev);
+      setCorrect((prev) => ++prev);
+    }
+  };
+  useEffect(() => {
+    setAnswer('');
+  }, [correcte]);
 
   return (
     <>
       <img src={`./img/quiz/${shuffleQuiz[quizNum].image}`} alt="" />
+      <button onClick={changeQuizHandler}>ボタン</button>
+      <input type="text" onChange={(e) => setAnswer(e.target.value)} value={answer} />
+      <p>{quizNum}</p>
     </>
   );
 }
