658f6278de6ff856697f8738b761569af3d31019
yuki-akiduki <yuki.akiduki1222@gmail.com>
2024-01-28 19:22:32 +0900

    fix：タイマーを全体で管理するため、usecontextを追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import Start from './components/Start/Start';
 import Layout from './layout/Layout';
 import { useState, createContext } from 'react';
 import Play from './components/play/Play';
-export const playScreen = createContext('start');
+export const playScreen = createContext();
+export const timer = createContext();
+export const timeRunning = createContext();
 
 function App() {
   // useStateで画面の出し分け処理
   const [playState, setPlayState] = useState('start');
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+
   const screen = (State) => {
     switch (State) {
       case 'start':
@@ -22,7 +27,11 @@ function App() {
     <>
       <Global styles={globalStyle} />
       <Layout>
-        <playScreen.Provider value={[playState, setPlayState]}>{screen(playState)}</playScreen.Provider>
+        <playScreen.Provider value={[playState, setPlayState]}>
+          <timer.Provider value={[time, setTime]}>
+            <timeRunning.Provider value={[isRunning, setIsRunning]}>{screen(playState)}</timeRunning.Provider>
+          </timer.Provider>
+        </playScreen.Provider>
       </Layout>
     </>
   );
--- src/components/play/Stopwatch.jsx
+++ src/components/play/Stopwatch.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useContext, useEffect, useState } from 'react';
+import { timer, timeRunning } from '../../App';
 export default function Timer() {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(true);
+  const [time, setTime] = useContext(timer);
+  const [isRunning, setIsRunning] = useContext(timeRunning);
 
   useEffect(() => {
     // タイマー処理
