188fe80aa5973ed75146eeb3c24fb6398bef4f19
yuki-akiduki <yuki.akiduki1222@gmail.com>
2024-01-28 11:24:28 +0900

    updte：不正解とパス時の処理

--- src/components/play/Game.jsx
+++ src/components/play/Game.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
 
 // クイズリスト
 const quizList = [
@@ -28,26 +29,59 @@ export default function Game() {
   const [quizNum, setQuizNum] = useState(0);
   const [correcte, setCorrect] = useState(0);
   const [answer, setAnswer] = useState('');
+  const [answerFiled, setAnswerFiled] = useState(false);
 
   // 正誤判定
-  const changeQuizHandler = () => {
+  const answerHandler = () => {
     if (shuffleQuizArr[quizNum].answer === answer) {
       setQuizNum((prev) => ++prev);
       setCorrect((prev) => ++prev);
+    } else {
+      // 不正解時は3秒ロス
+      return new Promise((resolve) => {
+        setAnswerFiled(true);
+        resolve();
+      }).then(() => {
+        setTimeout(() => {
+          setAnswerFiled(false);
+        }, 3000);
+      });
     }
+  };
+
+  // 問題をパス
+  const passHandeler = async () => {
+    setAnswerFiled(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setAnswerFiled(false);
+    setQuizNum((prev) => ++prev);
   };
 
   // 正解時、inputを空にする処理
   useEffect(() => {
     setAnswer('');
+    if (correcte === 2) {
+      setAnswerFiled(true);
+    }
   }, [correcte]);
 
   return (
     <>
       <img src={`./img/quiz/${shuffleQuizArr[quizNum].image}`} alt="" />
-      <button onClick={changeQuizHandler}>ボタン</button>
-      <input type="text" onChange={(e) => setAnswer(e.target.value)} value={answer} />
+      <button onClick={answerHandler} disabled={answerFiled}>
+        ボタン
+      </button>
+      <button onClick={passHandeler} disabled={answerFiled}>
+        パス
+      </button>
+      <input type="text" onChange={(e) => setAnswer(e.target.value)} value={answer} disabled={answerFiled} css={inputText} />
       <p>{quizNum}</p>
     </>
   );
 }
+
+const inputText = css`
+  &:disabled {
+    background: #000;
+  }
+`;
